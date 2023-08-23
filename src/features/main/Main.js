ca84a3b63f3d4f396dde3ace0e31130fe7a26174
Alerdo <alerdo23@gmail.com>
2023-08-23 20:04:08 +0100

    "Prepare to deploy in netlify"

--- src/features/main/Main.js
+++ src/features/main/Main.js
@@ -1,4 +1,3 @@
-import React, { useState } from 'react';
 import Week from './Week'
 import './Main.css';
 
--- src/features/main/Week.js
+++ src/features/main/Week.js
@@ -1,5 +1,5 @@
 import './Week.css';
-import { useState } from 'react';
+
 
 const Week = ({ weekData, currentLocation }) => {
   const { current } = weekData;
@@ -32,3 +32,47 @@ const Week = ({ weekData, currentLocation }) => {
 };
 
 export default Week;
+
+
+
+// import './Week.css';
+// import { useState } from 'react';
+
+// const Week = ({ weekData, currentLocation }) => {
+//   const { current } = weekData;
+
+
+//   //since the api call brings back only the date and not the specific week day(exmp: monday tuesday ect) I have converted the date into the belonging day
+// //   const  getDayOfWeek = (dateString) => {
+//   const  getDayOfWeek = (dateString) => {
+//     const date = new Date(dateString);
+//     const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+//     return days[date.getDay()];
+// }
+//   // Check if it's nighttime
+//   const isNight = !current.is_day;
+
+//   return (
+//     <>
+//       <h3>Weather for 3 days in {currentLocation}</h3>
+//       <div className="week">
+//         {weekData?.forecast?.forecastday.map((day) => (
+//           <div key={day.date} className="day">
+//             <p>{getDayOfWeek(day.date)}</p>
+//             <p>Date: {day.date}</p>
+//             <p>{day.day.condition.text}</p>
+//             <p>Temperature: {day.day.avgtemp_c}°C</p>
+//             {/* Update the class name for the icon */}
+//             <span
+//               className={`wi ${isNight ? 'wi-owm-night-' : 'wi-owm-day-'}${day.day.condition.code}`}
+//             ></span>
+//             {/* Display the day icon */}
+//             <img className='week-img' src={day.day.condition.icon} alt={current.condition.text} />
+//           </div>
+//         ))}
+//       </div>
+//     </>
+//   );
+// };
+
+// export default Week;
